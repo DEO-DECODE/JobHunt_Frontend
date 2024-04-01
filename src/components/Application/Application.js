2f7e0fb78a6c2b99ea814fe89f0b853838d45db6
Deo Raj <d.jrajsingh81@gmail.com>
2024-04-01 18:11:11 +0530

    Application component and PostJob component are created

---
+++ src/components/Application/Application.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { useAuth } from "../../context/auth.js";
+import toast from "react-hot-toast";
+import { useNavigate, useParams } from "react-router-dom";
+const Application = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [coverLetter, setCoverLetter] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [resume, setResume] = useState(null);
+  const [auth, setAuth] = useAuth();
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const handleFileChange = (e) => {
+    setResume(e.target.files[0]);
+  };
+  const handleApplication = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("phone", phone);
+    formData.append("address", address);
+    formData.append("coverLetter", coverLetter);
+    formData.append("resume", resume);
+    formData.append("jobId", id);
+    try {
+      const res = await fetch("api/v1/application/post", {
+        method: "POST",
+        body: productData,
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log("Error in form Submission");
+        console.log(data.message);
+      }
+      setName("");
+      setEmail("");
+      setCoverLetter("");
+      setPhone("");
+      setAddress("");
+      setResume("");
+      toast.success(data.message);
+      navigate("/job/getall");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  if (!auth.isAuthorized || (auth.user && auth.user.role === "Employer")) {
+    navigateTo("/");
+  }
+  return (
+    <section className="application">
+      <div className="container">
+        <h3>Application Form</h3>
+        <form onSubmit={handleApplication}>
+          <input
+            type="text"
+            value={name}
+            onChange={setName(e.target.value)}
+            placeholder="Your Name"
+          />
+          <input
+            type="email"
+            value={email}
+            onChange={setEmail(e.target.value)}
+            placeholder="Your Email"
+          />
+          <input
+            type="number"
+            value={phone}
+            onChange={setPhone(e.target.value)}
+            placeholder="Your Phone number"
+          />
+          <input
+            type="text"
+            value={address}
+            onChange={setAddress(e.target.value)}
+            placeholder="Your Address"
+          />
+          <textarea
+            rows={10}
+            value={coverLetter}
+            onChange={setCoverLetter(e.target.value)}
+            placeholder="Write a Cover Letter"
+          />
+          <div>
+            <label
+              style={{ textAlign: "start", display: "block", fontSize: "20px" }}
+            >
+              Select Resume
+            </label>
+            <input
+              type="file"
+              accept=".pdf, .jpg, .jpeg, .png"
+              onChange={handleFileChange}
+              style={{ width: "100%" }}
+            />
+          </div>
+          <button type="submit">Send Application</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Application;
---
+++ src/context/auth.js
@@ -0,0 +1,16 @@
+import { useState, createContext, useContext } from "react";
+const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({
+    isAuthorized: false,
+    user: {},
+  });
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = useContext(AuthContext);
+export { useAuth, AuthProvider };
